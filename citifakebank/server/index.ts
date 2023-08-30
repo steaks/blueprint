0fa14b27c8c1d0dba93b1bbea0843d1ca9b20336
Steven Wexler <steven.m.wexler@gmail.com>
2023-08-30 08:13:47 -0600

    Fix race conditions and other stuff

--- citifakebank/server/index.ts
+++
@@ -1,9 +0,0 @@
-import {serve} from "@blueprint/rx";
-import history from "./src/apps/history";
-import balance from "./src/apps/balance";
-import activity from "./src/apps/activity";
-import session from "./session";
-import atm from "./src/apps/atm";
-import team from "./src/apps/team";
-
-serve({team, history, balance, activity, atm}, session);
