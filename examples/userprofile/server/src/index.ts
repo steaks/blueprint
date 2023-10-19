09e70988f7bbbff6187c95e48a0dae3dda1d3675
Steven Wexler <steven.m.wexler@gmail.com>
2023-10-18 20:03:18 -0600

    Fix cors

--- examples/userprofile/server/src/index.ts
+++ examples/userprofile/server/src/index.ts
@@ -4,7 +4,7 @@ import userProfile from "./apps/userProfile";
 import session from "./session";
 
 const options = {
-    cors: {origin: process.env.CROSS_ORIGIN}
+    cors: {origin: process.env.CORS_ORIGIN}
 };
 
 serve({userProfile}, session, options);
