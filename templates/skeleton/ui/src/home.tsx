df78494528cb7dff421fc2803e2c45bd30d49f59
Steven Wexler <steven.m.wexler@gmail.com>
2024-03-10 19:46:26 -0600

    Add link

--- templates/skeleton/ui/src/home.tsx
+++ templates/skeleton/ui/src/home.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+
 const Home = () => {
   return (
     <>
       <h1>Welcome to Blueprint!</h1>
       <div>This is the blueprint skeleton template.</div>
-      <br />
+      <br/>
+      <div>Click <a href="helloWorld">here</a> to open the helloWorld app if you are following the getting started tutorial.</div>
     </>
   );
 };
