1afac05e0ef958e3b75d7f56413f92813bf664b1
Steven Wexler <steven.m.wexler@gmail.com>
2024-03-10 19:56:01 -0600

    Update hello world

--- templates/helloWorld/ui/src/home.tsx
+++ templates/helloWorld/ui/src/home.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 const Home = () => {
   return (
     <>
       <h1>Welcome to Blueprint!</h1>
-      <div>This is the blueprint hello world template. See the helloWorld app linked below.</div>
-      <br />
-      <a href="/helloWorld">Hello World</a>
+      <div>This is the blueprint skeleton template.</div>
+      <br/>
+      <div>Click <a href="helloWorld">here</a> to open the helloWorld app if you are following the getting started tutorial.</div>
     </>
   );
 };
