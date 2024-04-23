aa25339ef23d6bb353996421bcfad9251afb2e14
Steven Wexler <steven.m.wexler@gmail.com>
2024-04-23 00:25:04 -0600

    Touchups

---
+++ scripts/zipTemplates.sh
@@ -0,0 +1,7 @@
+cd templates
+tar -czvf gettingStarted-blueprint.tar.gz ./gettingStarted && mv gettingStarted-blueprint.tar.gz ~/developer/blueprint-templates
+tar -czvf helloWorld-blueprint.tar.gz ./helloWorld && mv helloWorld-blueprint.tar.gz ~/developer/blueprint-templates
+tar -czvf rectangle-blueprint.tar.gz ./rectangle && mv rectangle-blueprint.tar.gz ~/developer/blueprint-templates
+tar -czvf skeleton-blueprint.tar.gz ./skeleton && mv skeleton-blueprint.tar.gz ~/developer/blueprint-templates
+tar -czvf userProfile-blueprint.tar.gz ./userProfile && mv userProfile-blueprint.tar.gz ~/developer/blueprint-templates
+cd ..
